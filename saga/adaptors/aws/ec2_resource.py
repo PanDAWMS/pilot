d692cb619dae1b7429cd202ac415e90c38138b87
PalNilsson <Paul.Nilsson@cern.ch>
2015-01-22 17:00:42 +0100

    Merged with titan

--- saga/adaptors/aws/ec2_resource.py
+++ saga/adaptors/aws/ec2_resource.py
@@ -1,29 +1,14 @@
 
-<<<<<<< HEAD
-""" libcloud based EC2 resource adaptor """
-
-import os
-import re
-import time
-=======
 __author__    = "Andre Merzky, Ashley Z, Ole Weidner"
 __copyright__ = "Copyright 2012-2013, The SAGA Project"
 __license__   = "MIT"
 
 
 """ libcloud based EC2 resource adaptor """
->>>>>>> origin/titan
 
 import saga.adaptors.cpi.base
 import saga.adaptors.cpi.context
 import saga.adaptors.cpi.resource
-<<<<<<< HEAD
-from   saga.resource.constants import *
-
-
-ANY = COMPUTE | STORAGE
-
-=======
 
 from   saga.resource.constants import *
 ANY = COMPUTE | STORAGE
@@ -31,7 +16,6 @@ ANY = COMPUTE | STORAGE
 import re
 import os
 import time
->>>>>>> origin/titan
 
 SYNC_CALL  = saga.adaptors.cpi.decorators.SYNC_CALL
 ASYNC_CALL = saga.adaptors.cpi.decorators.ASYNC_CALL
@@ -138,11 +122,7 @@ class Adaptor (saga.adaptors.base.Base):
         # for id parsing
         self.id_re = re.compile ('^\[(.*)\]-\[(.*?)\]$')
 
-<<<<<<< HEAD
-=======
         self._default_contexts = list()
-
->>>>>>> origin/titan
 
     # ----------------------------------------------------------------
     #
@@ -195,24 +175,6 @@ class Adaptor (saga.adaptors.base.Base):
     #
     def _get_default_contexts (self) :
 
-<<<<<<< HEAD
-        # no default keypair in ec2 -- but lets see if we have default access
-        # information
-        if  not self._EC2_URL        and \
-            not self._EC2_ACCESS_KEY and \
-            not self._EC2_SECRET_KEY : 
-            # no default access info...
-            return []
-
-        # ok, lets pick up a default context from the EC2 default env vars
-        ctx = saga.Context ('ec2')
-
-        if self._EC2_URL        : ctx.server   = self._EC2_URL
-        if self._EC2_ACCESS_KEY : ctx.user_id  = self._EC2_ACCESS_KEY
-        if self._EC2_SECRET_KEY : ctx.user_key = self._EC2_SECRET_KEY
-
-        return [ctx]
-=======
         if  None == self._default_contexts :
 
             self._default_contexts = list()
@@ -235,7 +197,6 @@ class Adaptor (saga.adaptors.base.Base):
             self._default_contexts.append (ctx)
 
         return self._default_contexts
->>>>>>> origin/titan
 
 
     # ----------------------------------------------------------------
@@ -1149,9 +1110,5 @@ class EC2ResourceCompute (saga.adaptors.cpi.resource.Compute) :
     
 
 
-<<<<<<< HEAD
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-=======
 
->>>>>>> origin/titan
 
