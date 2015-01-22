b66c1ed119ee82053cc0eff15363500b8bb53e28
PalNilsson <Paul.Nilsson@cern.ch>
2015-01-22 17:56:46 +0100

    Merged with titan

--- saga/advert/__init__.py
+++ saga/advert/__init__.py
@@ -5,17 +5,9 @@ __license__   = "MIT"
 
 
 from saga.advert.constants  import *
-<<<<<<< HEAD
-from saga.advert.directory  import Directory
-from saga.advert.entry      import Entry
-
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-=======
 from saga.advert.entry      import Entry
 from saga.advert.directory  import Directory
 
 
 
 
->>>>>>> origin/titan
