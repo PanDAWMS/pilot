6e83086791c5279d4ff0c979352adf52d96266ee
PalNilsson <Paul.Nilsson@cern.ch>
2015-03-06 15:11:30 +0100

    New module for file handling added

---
+++ FileHandling.py
@@ -0,0 +1,46 @@
+# This module contains functions related to file handling.
+
+import os
+import time
+
+#from pUtil import tolog
+def tolog(s): print s
+
+def findLatestTRFLogFile(workdir):
+    """ Find out which is the latest log.* file """
+
+    last_log_file = ""
+
+    pattern = "log."
+    file_list = sortedLs(workdir, pattern)
+    if file_list != []:
+        last_log_file = file_list[-1]
+        tolog("Found payload log files: %s" % str(file_list))
+        tolog("File %s was the last log file that was updated" % (last_log_file))
+    else:
+        tolog("Did not find any log.* files, will return the trf stdout file instead")
+        last_log_file = "athena_stdout.txt" # WARNING!!! change to global file name for trf stdout
+
+    return last_log_file
+
+def sortedLs(path, pattern):
+    """ Sort the contents of directory 'path' using 'pattern' """
+
+    # Note: pattern is only a string, e.g. pattern = 'log.' will return a
+    # list with all files starting with 'log.' in time order
+    mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
+    file_list = []
+    try:
+        file_list = list(sorted(os.listdir(path), key=mtime))
+    except Exception, e:
+        tolog("!!WARNING!!3232!! Failed to obtain sorted file list: %s" % (e))
+
+    final_file_list = []
+    if file_list != []:
+        for f in file_list:
+            if f.startswith(pattern):
+                final_file_list.append(f)
+    return final_file_list
+
+print findLatestTRFLogFile(os.getcwd())
+
