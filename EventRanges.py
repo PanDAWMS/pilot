7045eca8df38a35a519a83416df08b6c344ff211
PalNilsson <Paul.Nilsson@cern.ch>
2015-03-24 14:45:00 +0100

    Minor cleanup

--- EventRanges.py
+++ EventRanges.py
@@ -37,15 +37,9 @@ def downloadEventRanges(jobId, jobsetID):
 def updateEventRange(event_range_id, eventRangeList, status='finished'):
     """ Update an event range on the Event Server """
 
-    # Return the server response (instruction to AthenaMP)
-    # Note: the returned message is a string (of a list of dictionaries). If it needs to be converted back to a list, use json.loads(message)
-
     tolog("Updating an event range..")
 
-    # message = "[{u'lastEvent': 2, u'LFN': u'mu_E50_eta0-25.evgen.pool.root',u'eventRangeID': u'130-2068634812-21368-1-1', u'startEvent': 2, u'GUID':u'74DFB3ED-DAA7-E011-8954-001E4F3D9CB1'}]"
-
-    message = ""
-    #url = "https://aipanda007.cern.ch:25443/server/panda"
+    # PanDA dev server: url = "https://aipanda007.cern.ch:25443/server/panda"
     url = "https://pandaserver.cern.ch:25443/server/panda"
     node = {}
     node['eventRangeID'] = event_range_id
@@ -53,7 +47,6 @@ def updateEventRange(event_range_id, eventRangeList, status='finished'):
     # node['cpu'] =  eventRangeList[1]
     # node['wall'] = eventRangeList[2]
     node['eventStatus'] = status
-    # tolog("node = %s" % str(node))
 
     # open connection
     ret = httpConnect(node, url, path=os.getcwd(), mode="UPDATEEVENTRANGE")
