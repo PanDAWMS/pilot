6cac2c800384bc434986f3d5fc74426347fca147
PalNilsson <Paul.Nilsson@cern.ch>
2015-04-02 10:35:56 +0200

    Using 10 lines as default in tail()

--- FileHandling.py
+++ FileHandling.py
@@ -80,7 +80,7 @@ def writeFile(filename, contents):
 
     return status
 
-def tail(f, lines=20):
+def tail(f, lines=10):
     """ Get the n last lines from file f """
 
     if lines == 0:
@@ -110,36 +110,6 @@ def tail(f, lines=20):
 
     tail_list = ''.join(data).splitlines()[-lines:]
     return '\n'.join(tail_list)
-
-def tail2(f, lines=20):
-    """ Get the n last lines from file f """
-
-    total_lines_wanted = lines
-
-    BLOCK_SIZE = 1024
-    f.seek(0, 2)
-    block_end_byte = f.tell()
-    lines_to_go = total_lines_wanted
-    block_number = -1
-    blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting
-                # from the end of the file
-    while lines_to_go > 0 and block_end_byte > 0:
-        if (block_end_byte - BLOCK_SIZE > 0):
-            # read the last block we haven't yet read
-            f.seek(block_number*BLOCK_SIZE, 2)
-            blocks.append(f.read(BLOCK_SIZE))
-        else:
-            # file too small, start from begining
-            f.seek(0,0)
-            # only read what was not read
-            blocks.append(f.read(block_end_byte))
-        lines_found = blocks[-1].count('\n')
-        lines_to_go -= lines_found
-        block_end_byte -= BLOCK_SIZE
-        block_number -= 1
-    all_read_text = ''.join(reversed(blocks))
-
-    return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
 
 # print findLatestTRFLogFile(os.getcwd())
 
