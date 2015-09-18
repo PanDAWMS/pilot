0f4903959721a5860e926e5cab161cd850ccc62c
PalNilsson <Paul.Nilsson@cern.ch>
2015-09-18 12:48:33 +0200

    Sending taskID with GETEVENTRANGES request

--- EventRanges.py
+++ EventRanges.py
@@ -9,7 +9,7 @@ def downloadEventRanges(jobId, jobsetID, taskID):
     # Return the server response (instruction to AthenaMP)
     # Note: the returned message is a string (of a list of dictionaries). If it needs to be converted back to a list, use json.loads(message)
 
-    tolog("Downloading new event ranges for jobId=%s and jobsetID=%s" % (jobId, jobsetID))
+    tolog("Downloading new event ranges for jobId=%s, taskID=%s and jobsetID=%s" % (jobId, taskID, jobsetID))
 
     # message = "[{u'lastEvent': 2, u'LFN': u'mu_E50_eta0-25.evgen.pool.root',u'eventRangeID': u'130-2068634812-21368-1-1', u'startEvent': 2, u'GUID':u'74DFB3ED-DAA7-E011-8954-001E4F3D9CB1'}]"
 
