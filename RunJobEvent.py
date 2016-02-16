51c3611718fa9384388c6b321da4a35d5f50f3ee
PalNilsson <Paul.Nilsson@cern.ch>
2016-02-16 20:05:05 +0100

    Abort functionality added

--- RunJobEvent.py
+++ RunJobEvent.py
@@ -94,6 +94,8 @@ class RunJobEvent(RunJob):
     __sending_event_range = False                # True while event range is being sent to payload
     __current_event_range = ""                   # Event range being sent to payload
     __useTokenExtractor = False                  # Should the TE be used?
+    __abort = False                              # Global bail-out
+    __tobekilled = False                         # Killed by server boolean
 
     # Getter and setter methods
 
@@ -456,6 +458,26 @@ class RunJobEvent(RunJob):
         """ Setter for __current_event_range """
 
         self.__current_event_range = current_event_range
+
+    def getAbort(self):
+        """ Getter for __abort """
+
+        return self.__abort
+
+    def setAbort(self, abort):
+        """ Setter for __abort """
+
+        self.__abort = abort
+
+    def getToBeKilled(self):
+        """ Getter for __tobekilled """
+
+        return self.__tobekilled
+
+    def setToBeKilled(self, tobekilled):
+        """ Setter for __tobekilled """
+
+        self.__tobekilled = tobekilled
 
     # Get/setters for the job object
 
@@ -1359,9 +1381,13 @@ class RunJobEvent(RunJob):
                                 # Did the updateEventRange back channel contain an instruction?
                                 if msg == "tobekilled":
                                     tolog("The PanDA server has issued a hard kill command for this job - AthenaMP will be killed (current event range will be aborted)")
+                                    self.setAbort(True)
+                                    self.setToBeKilled(True)
                                 if msg == "softkill":
                                     tolog("The PanDA server has issued a soft kill command for this job - current event range will be allowed to finish")
                                     self.sendMessage("No more events")
+                                    self.setAbort(True)
+
                         else:
                             tolog("!!WARNING!!1112!! Failed to create file metadata: %d, %s" % (ec, pilotErrorDiag))
             time.sleep(1)
@@ -2348,6 +2374,12 @@ if __name__ == "__main__":
                 i = 0
                 j = 0
                 for event_range in event_ranges:
+
+                    # do not continue if the abort has been set
+                    if runJob.getAbort():
+                        tolog("Aborting event range loop")
+                        break
+
                     # Send the event range to AthenaMP
                     tolog("Sending a new event range to AthenaMP (id=%s)" % (currentEventRangeIDs[j]))
                     runJob.setSendingEventRange(True)
@@ -2360,6 +2392,11 @@ if __name__ == "__main__":
 
                     # Wait until AthenaMP is ready to receive another event range
                     while not runJob.isAthenaMPReady():
+                        # do not continue if the abort has been set
+                        if runJob.getAbort():
+                            tolog("Aborting AthenaMP loop")
+                            break
+
                         # Take a nap
                         if i%10 == 0:
                             tolog("Event range loop iteration #%d" % (i))
@@ -2446,20 +2483,26 @@ if __name__ == "__main__":
                         break
 
         # Wait for AthenaMP to finish
-        i = 0
         kill = False
-        while athenaMPProcess.poll() is None:
-            tolog("Waiting for AthenaMP to finish (#%d)" % (i))
-            if i > max_wait:
-                # Stop AthenaMP
-                tolog("Waited long enough - Stopping AthenaMP process")
-                athenaMPProcess.kill()
-                tolog("(Kill signal SIGTERM sentto AthenaMP - jobReport might get lost)")
-                kill = True
-                break
+        if runJob.getToBeKilled():
+            tolog("The pilot has been instructed by the server to kill AthenaMP")
+            athenaMPProcess.kill()
+            tolog("(Kill signal SIGTERM sent to AthenaMP - jobReport might get lost)")
+            kill = True
+        else:
+            i = 0
+            while athenaMPProcess.poll() is None:
+                tolog("Waiting for AthenaMP to finish (#%d)" % (i))
+                if i > max_wait:
+                    # Stop AthenaMP
+                    tolog("Waited long enough - Stopping AthenaMP process")
+                    athenaMPProcess.kill()
+                    tolog("(Kill signal SIGTERM sent to AthenaMP - jobReport might get lost)")
+                    kill = True
+                    break
 
-            time.sleep(60)
-            i += 1
+                time.sleep(60)
+                i += 1
 
         if not kill:
             tolog("AthenaMP has finished")
