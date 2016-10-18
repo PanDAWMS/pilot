24740f274cf852c29550750ca9fd997cc0cd6479
Alexey Anisenkov <Alexey.Anisenkov@cern.ch>
2016-10-18 21:11:22 +0200

    pandaID tag fixed

--- PandaServerClient.py
+++ PandaServerClient.py
@@ -276,7 +276,7 @@ class PandaServerClient:
         if self.__jobSchedulerId:
             node['schedulerID'] = self.__jobSchedulerId
         if self.__pilotId:
-            use_newmover = readpar('use_newmover')
+            use_newmover = str(readpar('use_newmover')).lower() in ["1", "true"]
             use_newmover_tag = 'NEWMOVER-%s' % ('ON' if use_newmover else 'OFF')
             tolog("Checking if new site movers workflow is enabled: use_newmover=%s" % use_newmover)
 
