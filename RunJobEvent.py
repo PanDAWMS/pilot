f02883ef830397e8cf3017f29adad19e8bf50c22
Wen Guan <wen.guan@cern.ch>
2016-11-17 15:03:18 +0100

    download events before startAthenaMP

--- RunJobEvent.py
+++ RunJobEvent.py
@@ -999,11 +999,12 @@ class RunJobEvent(RunJob):
         # change to sys.exit?
         os._exit(self.__job.result[2]) # pilotExitCode, don't confuse this with the overall pilot exit code,
                                        # which doesn't get reported back to panda server anyway
-    def failJob(self, transExitCode, pilotExitCode, job, ins=None, pilotErrorDiag=None, docleanup=True):
+    def failJob(self, transExitCode, pilotExitCode, job, ins=None, pilotErrorDiag=None, docleanup=True, pilot_failed=True):
         """ set the fail code and exit """
 
         job.setState(["failed", transExitCode, pilotExitCode])
-        job.subStatus = 'pilot_failed'
+        if pilot_failed:
+            job.subStatus = 'pilot_failed'
         if pilotErrorDiag:
             job.pilotErrorDiag = pilotErrorDiag
         tolog("Will now update local pilot TCP server")
@@ -2836,6 +2837,15 @@ if __name__ == "__main__":
         #runCommandList[0] = runCommandList[0].replace(inputFile, turl)
         #tolog("Replaced '%s' with '%s' in the run command" % (inputFile, turl))
 
+        # download event ranges before athenaMP
+        # Pilot will download some event ranges from the Event Server
+        message = downloadEventRanges(job.jobId, job.jobsetID, job.taskID, numRanges=job.coreCount)
+        # Create a list of event ranges from the downloaded message
+        first_event_ranges = runJob.extractEventRanges(message)
+        if first_event_ranges is None or first_event_ranges == []:
+            tolog("No more events. will finish this job directly")
+            runJob.failJob(0, error.ERR_NOEVENTS, job, pilotErrorDiag="No events before start AthenaMP", pilot_failed=False)
+
         # Create and start the AthenaMP process
         t0 = os.times()
         tolog("t0 = %s" % str(t0))
@@ -2873,11 +2883,15 @@ if __name__ == "__main__":
             # the boolean will be set to true in the listener after the "Ready for events" message is received from the client
             if runJob.isAthenaMPReady():
 
-                # Pilot will download some event ranges from the Event Server
-                message = downloadEventRanges(job.jobId, job.jobsetID, job.taskID, numRanges=job.coreCount)
+                if first_event_ranges:
+                    event_ranges = first_event_ranges
+                    first_event_ranges = None
+                else:
+                    # Pilot will download some event ranges from the Event Server
+                    message = downloadEventRanges(job.jobId, job.jobsetID, job.taskID, numRanges=job.coreCount)
 
-                # Create a list of event ranges from the downloaded message
-                event_ranges = runJob.extractEventRanges(message)
+                    # Create a list of event ranges from the downloaded message
+                    event_ranges = runJob.extractEventRanges(message)
 
                 # Are there any event ranges?
                 if event_ranges == []:
@@ -3270,6 +3284,8 @@ if __name__ == "__main__":
         runJob.setFinalESStatus(job)
         job.setState([job.jobState, job.result[1], job.result[2]])
         runJob.setJobState(job.jobState)
+        job.nEvents, job.nEventsW, job.nEventsFailed, job.nEventsFailedStagedOut = runJob.getNEvents()
+        tolog("nevents = %s, neventsW = %s, neventsFailed = %s, nEventsFailedStagedOut=%s" % (job.nEvents, job.nEventsW, job.nEventsFailed, job.nEventsFailedStagedOut))
         rt = RunJobUtilities.updatePilotServer(job, runJob.getPilotServer(), runJob.getPilotPort(), final=True)
 
         tolog("Done")
