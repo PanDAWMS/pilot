460351e7f4f0c8c0ef16688c0198634cdd822c08
PalNilsson <Paul.Nilsson@cern.ch>
2017-05-09 14:43:43 +0200

    Removed process id and time stamp from site workdir in getWorkDir()

--- Site.py
+++ Site.py
@@ -18,5 +18,6 @@ class Site:
     def getWorkDir(self):
         """ When there are multi-jobs, the site workdir needs to be updated """
 
-        jobworkdir = "Panda_Pilot_%d_%s" % (os.getpid(), str(int(time.time())))
+        jobworkdir = "Panda_Pilot"
+        # jobworkdir = "Panda_Pilot_%d_%s" % (os.getpid(), str(int(time.time())))
         return os.path.join(self.wntmpdir, jobworkdir) # this pilots' workdir
