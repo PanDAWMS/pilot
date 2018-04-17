5748a5ec277ff6394e4ea0add6e0414aaca27db3
PalNilsson <atlas-adc-pilot@cern.ch>
2018-04-17 11:04:42 +0200

    Avoiding adding archive to outFiles if it is already listed as an output file

--- Job.py
+++ Job.py
@@ -842,30 +842,34 @@ class Job:
         """ Add the zip archives to the output file lists """
 
         for archive in zip_map.keys():
-            content_files = zip_map[archive]
-            pUtil.tolog('Processing files from archive %s: %s' % (archive, str(content_files)))
+            # do not add the archive if it is already listed as an output file
+            if archive not in outFiles:
+                content_files = zip_map[archive]
+                pUtil.tolog('Processing files from archive %s: %s' % (archive, str(content_files)))
 
-            # Find the corresponding destinationDblock, destinationDBlockToken, scopeOut for the content_files
-            # so we can use them for the archive itself
-            found = False
-            for inFile in inFiles:
-                if content_files[0] == inFile: # assume same info for all input files in this archive, use first file
-                    found = True
-                    break
-            if found:
-                archiveDestinationDblock = dispatchDblock[0]
-                archiveDestinationDBlockToken = dispatchDBlockToken[0]
-                archiveScopeOut = scopeIn[0]
+                # Find the corresponding destinationDblock, destinationDBlockToken, scopeOut for the content_files
+                # so we can use them for the archive itself
+                found = False
+                for inFile in inFiles:
+                    if content_files[0] == inFile: # assume same info for all input files in this archive, use first file
+                        found = True
+                        break
+                if found:
+                    archiveDestinationDblock = dispatchDblock[0]
+                    archiveDestinationDBlockToken = dispatchDBlockToken[0]
+                    archiveScopeOut = scopeIn[0]
+                else:
+                    pUtil.tolog("!!WARNING!!3434!! Did not find zip content file among output files")
+                    archiveDestinationDblock = "UNKNOWN"
+                    archiveDestinationDBlockToken = "UNKNOWN"
+                    archiveScopeOut = "UNKNOWN"
+
+                outFiles.append(archive)
+                destinationDblock.append(archiveDestinationDblock)
+                destinationDBlockToken.append(archiveDestinationDBlockToken)
+                scopeOut.append(archiveScopeOut)
             else:
-                pUtil.tolog("!!WARNING!!3434!! Did not find zip content file among output files")
-                archiveDestinationDblock = "UNKNOWN"
-                archiveDestinationDBlockToken = "UNKNOWN"
-                archiveScopeOut = "UNKNOWN"
-
-            outFiles.append(archive)
-            destinationDblock.append(archiveDestinationDblock)
-            destinationDBlockToken.append(archiveDestinationDBlockToken)
-            scopeOut.append(archiveScopeOut)
+                pUtil.tolog('Archive %s is already listed as an output file' % archive)
 
         return outFiles, destinationDblock, destinationDBlockToken, scopeOut
 
