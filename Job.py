1413e97f6385277baf2147eb2cdf821a4e3ca6a0
palnilsson <Paul.Nilsson@cern.ch>
2018-05-30 15:55:25 +0200

    Added https:// protocol to allowed_replica_schemas

--- Job.py
+++ Job.py
@@ -1294,7 +1294,7 @@ class FileSpec(object):
 
         if ensure_replica:
 
-            allowed_replica_schemas = ['root://', 'dcache://', 'dcap://', 'file://']
+            allowed_replica_schemas = ['root://', 'dcache://', 'dcap://', 'file://', 'https://']
 
             if self.turl:
                 if True not in set([self.turl.startswith(e) for e in allowed_replica_schemas]):
