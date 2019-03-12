9dd434aac2dc67fcc80454377ed0b75f3e77caed
Paul Nilsson <paul.nilsson@cern.ch>
2019-03-12 15:59:47 +0100

    New version of detect_client_location()

--- pilot.py
+++ pilot.py
@@ -2636,6 +2636,42 @@ def getsetWNMem(memory):
 
 def detect_client_location(site):
     """
+    Open a UDP socket to a machine on the internet, to get the local IPv4 and IPv6
+    addresses of the requesting client.
+    Try to determine the sitename automatically from common environment variables,
+    in this order: SITE_NAME, ATLAS_SITE_NAME, OSG_SITE_NAME. If none of these exist
+    use the fixed string 'ROAMING'.
+    """
+
+    ip = '0.0.0.0'
+    try:
+        import socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        pass
+
+    ip6 = '::'
+    try:
+        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        s.connect(("2001:4860:4860:0:0:0:0:8888", 80))
+        ip6 = s.getsockname()[0]
+    except Exception:
+        pass
+
+#    site = os.environ.get('SITE_NAME',
+#                          os.environ.get('ATLAS_SITE_NAME',
+#                                         os.environ.get('OSG_SITE_NAME',
+#                                                        'ROAMING')))
+
+    return {'ip': ip,
+            'ip6': ip6,
+            'fqdn': socket.getfqdn(),
+            'site': site}
+
+def detect_client_location_old(site):
+    """
     Open a UDP socket to a machine on the internet, to get the local IP address
     of the requesting client.
     Try to determine the sitename automatically from common environment variables,
